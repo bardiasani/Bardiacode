90443075b14513cc5ca8eba8b10633ea8f276b34
BARDIA <61330844+bardiasani@users.noreply.github.com>
2023-07-28 10:52:04 +0200

    Update index.js

--- constants/index.js
+++ constants/index.js
@@ -1,4 +1,4 @@
-export const TOKEN_CONTRACT_ADDRESS = "0xfF5cC26d33A84F10760718b207B1765744D0fB0A";
+export const TOKEN_CONTRACT_ADDRESS = "0xf97c4b42a530d5e8609d7d154fcc5d7395ec981d";
 export const TOKEN_CONTRACT_ABI = [
 	{
 		"anonymous": false,
